870fa5519fb9d2988571501f2a4a9f9022327c65
Tom Gagliano <tgaglianoll@gmail.com>
2023-05-10 14:09:17 -0400

    add npm math and add webpack.config file

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+var moment = require('moment'); // require
+moment().format(); 
 
 console.log("Hello how are you?")
 
 var datetime = moment().startOf("day").fromNow();
 console.log(moment().format('LLLL'));
+
+import { sqrt } from 'mathjs';
+
+console.log(sqrt(-4).toString());
+
